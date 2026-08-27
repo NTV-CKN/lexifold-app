fbda121cb108d46abb4d183208069410193b08fb
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-08-27 18:42:25 +0700

    Cấu hình l10n.yaml, thêm file app_vi.arb

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lexifold/utils/theme_utils.dart';
+
+import 'l10n/app_localizations.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      locale: const Locale("vi"),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+
       theme: ThemeUtils.lightTheme,
       darkTheme: ThemeUtils.darkTheme,
       themeMode: ThemeMode.system,
@@ -19,7 +30,7 @@ class LexiFoldApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Hello world!')),
-      body: Text("LexiFoldApp"),
+      body: Text(AppLocalizations.of(context)?.appName ?? "Unknown"),
     );
   }
 }
